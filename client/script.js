0c50351d95645498dcc03c3a8ead54a4dba1b6f1
TSSxEmber <vincent.fezzuoglio@protonmail.com>
2023-09-04 09:45:19 -0400

    modifying files

--- client/script.js
+++ client/script.js
@@ -68,7 +68,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  const response = await fetch('http://localhost:5000',{
+  const response = await fetch('https://chat-gbt-web-app-clone.onrender.com',{
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
